c614219fbf7f38d18614b31539f988ef89f9ca2e
Mouhamadou Diamanka <diamanka.sn@gmail.com>
2024-08-31 22:57:36 +0000

    fetch data

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,9 +1,41 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react"
 
-import { useSearchParams } from "next/navigation"
-export default function PostID({params, searchParams}:any){
-    // const searchParams = useSearchParams() 
-    // const name = searchParams.get('name')
-    // console.log(name)
-    return <main>Post {params.id} - name {searchParams.name}</main>
+type Post = {
+    title?: string
+    description?: string
+}
+export default function PostID({ params }: any) {
+    const [post, setPost] = useState<Post | null>(null)
+    const [loading, setLoading] = useState(false)
+    const getPostById = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(`http://localhost:3000/api/posts/${params.id}`, {
+                method: 'GET',
+                next:{
+                    revalidate: 5000
+                }
+            })
+            debugger
+            if (response) {
+                const { post } = await response.json()
+                if (post) setPost(post)
+            }
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+    }
+    useEffect(() => {
+        getPostById()
+    }, [])
+    return <main>
+        {loading && <div>loading</div>}
+        {!loading && <div>
+            {post && <h1>{post.title}</h1>}
+            {post && <span>{post.description}</span>}
+        </div>}
+    </main>
 }
