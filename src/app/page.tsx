e734fd2c38a5d0fb986f645171e43038c4fbea31
Mouhamadou Diamanka <diamanka.sn@gmail.com>
2024-08-31 23:39:34 +0000

    notification or toast sonner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,42 @@
 "use client"
-import Header from "@/components/Header";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
+import { toast } from "sonner";
 export default function Home() {
   const [loading, setLoading] = useState(false);
-  const fetchData = async ()=>{
+
+  const fetchData = async () => {
     try {
       setLoading(true)
       const response = await fetch('/api/users', {
-        headers:{
-          Accept:"application/json",
-          method:"GET"
+        headers: {
+          Accept: "application/json",
+          method: "GET"
         }
       })
 
-      if(response){
+      if (response) {
         const data = await response.json();
         console.log(data);
       }
     } catch (error) {
       console.log(error)
-    } finally{
+    } finally {
       setLoading(false)
     }
   }
-  const name = "modou"
   return (
     <main>
-      <Header name={name} age={45} isMen={true}/>
       Hello world
       <div className="w-[500px]">
-      <Image src="/next.svg" width={300} height={90} alt="default image"/>
+        <Image src="/next.svg" width={300} height={90} alt="default image" />
       </div>
       <button onClick={fetchData}>Call api</button>
+      <Link href="/posts">Link to post pages</Link>
+      <div>
+        <button onClick={() => toast.success('success')} className="px-5 py-3 text-white bg-blue-500 rounded">Notify me</button>
+      </div>
     </main>
   );
 }
